a1a98698c8b09511b45b39f087dbfaf0b1c45cef
Gregory Sheerin <S00126962@mail.itsligo.ie>
2018-11-19 10:07:33 +0000

    added try catch to pawn string reading

--- WowSlack/itemCalc/itemCalc.js
+++ WowSlack/itemCalc/itemCalc.js
@@ -1,159 +1,157 @@
-
 const blizzard = require('blizzard.js').initialize({
     apikey: 'qupb7zxzkdtzzzt87nnkyny29b289aw9'
 });
 const itemMap = require('./itemStatMapping.json'); //call in the mapping for data here
-const itemSlots = ["head","neck","shoulder","back","chest","wrist","hands","waist","legs","feet","finger1","finger2","trinket1","trinket2","mainHand","offHand"];
+const itemSlots = ["head", "neck", "shoulder", "back", "chest", "wrist", "hands", "waist", "legs", "feet", "finger1", "finger2", "trinket1", "trinket2", "mainHand", "offHand"];
 
 
-function GetOverallItemValue(itemToCalc,StatWeights)
-{
+function GetOverallItemValue(itemToCalc, StatWeights) {
     var ItemValue = 0; //var to hold the overallValue for the item
     for (var key in itemToCalc) {
         if (itemToCalc.hasOwnProperty(key)) {
 
-            if(key == "Strength" ||key == "Intellect" ||key == "Agility")
-            {
-                ItemValue += calcItemStatValue(StatWeights["PrimWeight"],itemToCalc[key]);
-            }
-
-            else
-            {
-                for(var WeightKey in StatWeights)
-                {
-                    if(WeightKey.includes(key))
-                    {
-                      // console.log(WeightKey +":" +StatWeights[WeightKey] + " " +  "-> " + key + itemToCalc[key])
-                       ItemValue += calcItemStatValue(StatWeights[WeightKey],itemToCalc[key])
+            if (key == "Strength" || key == "Intellect" || key == "Agility") { //deal with items that just use the Single terms,need to handle multi stats like 71 in mapping
+                ItemValue += calcItemStatValue(StatWeights["PrimWeight"], itemToCalc[key]);
+            } else {
+                for (var WeightKey in StatWeights) {
+                    if (WeightKey.includes(key)) {
+                        // console.log(WeightKey +":" +StatWeights[WeightKey] + " " +  "-> " + key + itemToCalc[key])
+                        ItemValue += calcItemStatValue(StatWeights[WeightKey], itemToCalc[key])
                     }
                 }
             }
         }
-      return ItemValue;
+        return ItemValue;
     }
 
 
 }
-function calcItemStatValue(itemStatWeight,ItemStatAmount)
-{
+
+function calcItemStatValue(itemStatWeight, ItemStatAmount) {
     //return the full value in order to get the most correct results when comparing
     return (itemStatWeight * ItemStatAmount);
 }
 
-function GetCharAPI(Name,Sever,Region)
-{
+function GetCharAPI(Name, Sever, Region) {
     blizzard.wow.character(['profile', 'items'], {
-        realm: Sever,
-        name: Name,
-        origin: Region
-      })
-      .then(response => {
-        console.log(response.data.items.shoulder);
-    })
+            realm: Sever,
+            name: Name,
+            origin: Region
+        })
+        .then(response => {
+            console.log(response.data.items.shoulder);
+        })
 }
 
-function GenerateCharItemTemplate(Name,Sever,Region)
-{
-    var PawnValues =readPawnString("( Pawn: v1: \"Keyboardwárr-Fury\": Class=Warrior, Spec=Fury, Strength=1.46, Ap=1.37, CritRating=1.21, HasteRating=1.68, MasteryRating=1.35, Versatility=1.21, Dps=5.39 )")
-
+function GenerateCharItemTemplate(Name, Sever, Region) {
+    var PawnValues = readPawnString("( Pawn: v1: \"Keyboardwárr-Fury\": Class=Warrior, Spec=Fury,reallyCoolStat=1.41 Strength=1.44, Ap=1.36, CritRating=1.19, HasteRating=1.66, MasteryRating=1.32, Versatility=1.19, Dps=5.39 ")
+    
     var CharObj = {};
     blizzard.wow.character(['profile', 'items'], {
-        realm: Sever,
-        name: Name,
-        origin: Region
-      })
-      .then(response => {
+            realm: Sever,
+            name: Name,
+            origin: Region
+        })
+        .then(response => {
 
-        itemSlots.forEach(function(itemSlotName) {
+            itemSlots.forEach(function (itemSlotName) {
 
-            var statsObj = GenerateItemValue(response.data.items[itemSlotName].stats);     
-            CharObj[itemSlotName] = statsObj;
-            var itemValue = GetOverallItemValue(CharObj[itemSlotName],PawnValues)
+                var statsObj = GenerateItemValue(response.data.items[itemSlotName].stats);
+                CharObj[itemSlotName] = statsObj;
+                var itemValue = GetOverallItemValue(CharObj[itemSlotName], PawnValues)
 
-            CharObj[itemSlotName].OverAllValue = itemValue;
-         });
-         console.log(CharObj)
-    })
-    
+                CharObj[itemSlotName].OverAllValue = itemValue;
+            });
+            console.log(CharObj)
+        })
+
 }
 
-function GenerateItemValue(item)
-{
+function GenerateItemValue(item) {
     var returnObj = {};
     item.forEach(element => {
-                
-        var Name = itemMap[element.stat];
-        var StatAmount = element.amount;   
-        returnObj[Name] = StatAmount   
 
-     });
+        var Name = itemMap[element.stat];
+        var StatAmount = element.amount;
+        returnObj[Name] = StatAmount
+
+    });
     return returnObj;
 }
 
 //function to pull out the stat weights for a character
-function readPawnString(Pawnstring)
-{
+function readPawnString(Pawnstring) {
     //obj to store all the weights in,default everything to zero in the case a character doesnt use a certain stat
     //eg Dps wont care about leech,chances are they wont be in the pawn string
     var returnObj = {
-        "PrimWeight":0 ,
-        "HasteWeight":0 ,
-        "CritWeight":0 ,
-        "ArmorWeight":0 ,
-        "LeechWeight":0 ,
-        "MasteryWeight":0 ,
-        "StaminaWeight":0 ,
-        "VersatilityWeight":0 ,
-        "WepDpsWeight":0 };
+        "PrimWeight": 0,
+        "HasteWeight": 0,
+        "CritWeight": 0,
+        "ArmorWeight": 0,
+        "LeechWeight": 0,
+        "MasteryWeight": 0,
+        "StaminaWeight": 0,
+        "VersatilityWeight": 0,
+        "WepDpsWeight": 0
+    };
 
-        //since we dont care about the Pawn version or name,just remove everything before the stats
-       while (Pawnstring.includes(":")) {
-        Pawnstring = Pawnstring.substring(Pawnstring.indexOf(":") + 1);
-       }
-
+    try {
+        while (Pawnstring.includes(":")) {
+            Pawnstring = Pawnstring.substring(Pawnstring.indexOf(":") + 1);
+        }
+    
         //now,spilt up the array on commas
-        var PawnArray = Pawnstring.split(","); //I could just send this back,but I need to make sure everything has a default value
-     
+        var PawnArray = Pawnstring.split(","); 
+    
         //this is abit messy,maybe a way to loop the whole thing
         //now look for particlar stats in the array and get the value out
         var PrimWeight = PawnArray.filter(s => s.includes('Strength') || s.includes('Intellect') || s.includes('Agility')); //allows me to treat all these are the same value
-        if (PrimWeight && PrimWeight.length ) { //make sure that there is stuff in the array
+        if (PrimWeight && PrimWeight.length) { //make sure that there is stuff in the array
             returnObj.PrimWeight = PrimWeight[0].split("=")[1]; //get the decimal value out
         }
         var CritRating = PawnArray.filter(s => s.includes('Crit'));
-        if (CritRating && CritRating.length ) {
+        if (CritRating && CritRating.length) {
             returnObj.CritWeight = CritRating[0].split("=")[1];
-        } 
+        }
         var HasteWeight = PawnArray.filter(s => s.includes('Haste'));
-        if (HasteWeight && HasteWeight.length ) {
+        if (HasteWeight && HasteWeight.length) {
             returnObj.HasteWeight = HasteWeight[0].split("=")[1];
         }
         var LeechWeight = PawnArray.filter(s => s.includes('Leech'));
-        if (LeechWeight && LeechWeight.length ) {
+        if (LeechWeight && LeechWeight.length) {
             returnObj.LeechWeight = LeechWeight[0].split("=")[1];
         }
         var MasteryWeight = PawnArray.filter(s => s.includes('Mast'));
-        if (MasteryWeight && MasteryWeight.length ) {
+        if (MasteryWeight && MasteryWeight.length) {
             returnObj.MasteryWeight = MasteryWeight[0].split("=")[1];
         }
         var StaminaWeight = PawnArray.filter(s => s.includes('Stam'));
-        if (StaminaWeight && StaminaWeight.length ) {
+        if (StaminaWeight && StaminaWeight.length) {
             returnObj.StaminaWeight = StaminaWeight[0].split("=")[1];
         }
         var ArmorWeight = PawnArray.filter(s => s.includes('Armor'));
-        if (ArmorWeight && ArmorWeight.length ) {
+        if (ArmorWeight && ArmorWeight.length) {
             returnObj.ArmorWeight = ArmorWeight[0].split("=")[1];
         }
         var VersatilityWeight = PawnArray.filter(s => s.includes('Vers'));
-        if (VersatilityWeight && VersatilityWeight.length ) {
+        if (VersatilityWeight && VersatilityWeight.length) {
             returnObj.VersatilityWeight = VersatilityWeight[0].split("=")[1];
         }
         var WepDpsWeight = PawnArray.filter(s => s.includes('Dps'));
-        if (WepDpsWeight && WepDpsWeight.length ) {
+        if (WepDpsWeight && WepDpsWeight.length) {
             returnObj.WepDpsWeight = WepDpsWeight[0].split("=")[1];
         }
-        return returnObj; //finally return the object
+        
+    } catch (error) {
+        console.log(error.message)
+    }
+    //since we dont care about the Pawn version or name,just remove everything before the stats
+
+    return returnObj; //finally return the object
 }
 
-GenerateCharItemTemplate("KeyBoardwárr","Silvermoon","eu")
+
+//GetCharAPI("KeyBoardwárr", "Silvermoon", "eu")
+var PawnValues = readPawnString("( Pawn: v1: \"Keyboardwárr-Fury\": Class=Warrior, Spec=Fury,reallyCoolStat=1.41 Strength=1.44, Ap=1.36, CritRating=1.19, HasteRating=1.66, MasteryRating=1.32, Versatility=1.19, Dps=5.39 ")
+   console.log(PawnValues)
 //var stats =readPawnString("( Pawn: v1: \"Keyboardwárr-Fury\": Class=Warrior, Spec=Fury, Strength=1.46, Ap=1.37, CritRating=1.21, HasteRating=1.68, MasteryRating=1.35, Versatility=1.21, Dps=5.39 )")
